a9f1f74b304e26f7a54fb9f585d06b8341f351c8
svjacinto <syrejacinto.se@gmail.com>
2024-09-26 09:37:25 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/thingsboard_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.thingsboard_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,8 @@
+abstract class ThingsboardAppConstants {
+  // static const thingsBoardApiEndpoint = 'http://localhost:8080';
+  static const thingsBoardApiEndpoint = 'https://demo.thingsboard.io';
+  static const thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
+
+  /// Not for production (only for debugging)
+  static const thingsboardOAuth2AppSecret = 'Your app secret here';
+}
